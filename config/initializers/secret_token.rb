2d3ad72beef5b12cd6ba98e9ee20c2365663f7a1
Marc Stevens <mstevens@colum.edu>
2013-03-02 10:35:44 -0600

    added devise, carrierwave, and acts_as_list

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Minxtape::Application.config.secret_token = '00092aa9470005658d64cff12dc8d84a49dd00454ce2a800fa6f00ac128f9385f5462d088370fd7ba59a89be4fc13033f585d753d42ae48be087bd9cdd60a9ae'
