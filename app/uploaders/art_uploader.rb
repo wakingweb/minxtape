38c6f5121f6713d836fa3b27aaaf717d6a9f4950
Marc Stevens <marc@wakingweb.com>
2013-03-08 20:17:17 -0600

    style updates

--- app/uploaders/art_uploader.rb
+++ app/uploaders/art_uploader.rb
@@ -25,7 +25,7 @@ class ArtUploader < CarrierWave::Uploader::Base
   process :resize_to_fit => [800, 800]
 
   version :thumb do
-    process :resize_to_fill => [200,200]
+    process :resize_to_fill => [300,200]
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
