ec3d6a943738df17e751f682b5021752a1f4be20
Marc Stevens <marc@wakingweb.com>
2016-02-04 16:38:42 -0600

    Refactor

--- app/models/track.rb
+++ app/models/track.rb
@@ -2,6 +2,6 @@ class Track < ActiveRecord::Base
   attr_accessible :mix_id, :artist, :name, :audio, :position
   belongs_to :mix
 
-  acts_as_list :scope => :mix
+  acts_as_list scope: :mix
   mount_uploader :audio, AudioUploader
 end
