06fe465371cf272e7d39f1c6f0c74b8542120601
Marc Stevens <marc@wakingweb.com>
2016-02-07 20:40:28 -0600

    Fixes facebook authentication callback

--- config/routes.rb
+++ config/routes.rb
@@ -11,10 +11,6 @@ Minxtape::Application.routes.draw do
   end
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-
-  # devise_scope :user do
-  #   delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
-  # end
   
   resources :users do
     resources :mixes
