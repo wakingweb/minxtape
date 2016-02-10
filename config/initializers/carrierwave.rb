5d251963f5bb9b72687e67d6fe3c2edfe48437bf
Marc Stevens <marc@wakingweb.com>
2016-02-10 14:04:12 -0600

    Moves sensitive info to environment variables

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,9 +3,9 @@ Rails.env.production? ? (bucket_name = "minxtape-production") : (bucket_name = "
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider:              'AWS',                                       # required
-    aws_access_key_id:     'AKIAIC2NAMMXMCERT24A',                      # required
-    aws_secret_access_key: '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',  # required
+    aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],                    # required
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],                # required
     region:                'us-west-2',
   }
-  config.fog_directory  = bucket_name                                       # required
+  config.fog_directory  = bucket_name                                   # required
 end
