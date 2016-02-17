c17c8a01a8922d36935956229b1551d6206084ef
Marc Stevens <marc@wakingweb.com>
2016-02-16 22:12:12 -0600

    Refreshes schema

--- db/schema.rb
+++ db/schema.rb
@@ -17,37 +17,37 @@ ActiveRecord::Schema.define(version: 20160208022124) do
   enable_extension "plpgsql"
 
   create_table "mixes", force: :cascade do |t|
-    t.string   "title",       limit: 255
+    t.string   "title"
     t.text     "description"
     t.integer  "user_id"
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
-    t.string   "art",         limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "art"
   end
 
   create_table "tracks", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.string   "artist",     limit: 255
+    t.string   "name"
+    t.string   "artist"
     t.integer  "mix_id"
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
-    t.string   "audio",      limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "audio"
     t.integer  "position"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "", null: false
-    t.string   "encrypted_password",     limit: 255, default: "", null: false
-    t.string   "reset_password_token",   limit: 255
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                      default: 0
+    t.integer  "sign_in_count",          default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
-    t.datetime "created_at",                                      null: false
-    t.datetime "updated_at",                                      null: false
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "provider"
     t.string   "uid"
   end
